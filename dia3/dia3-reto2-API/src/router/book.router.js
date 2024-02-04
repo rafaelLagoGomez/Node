dee84224f0834c48c2b42e5409d8b85a93e28a97
= <=>
2024-02-04 20:53:06 +0100

    subo retos dia3 node express-API

---
+++ dia3/dia3-reto2-API/src/router/book.router.js
@@ -0,0 +1,18 @@
+
+// Aquí creamos en endpoints de las rutas.
+
+const { Router } = require('express');
+const router = Router();
+const bookController = require("../controller/book.controller")
+
+router.get('/', bookController.welcome);
+
+router.get('/book', bookController.getBook);
+
+router.post('/book', bookController.createBook);
+
+router.put('/book', bookController.updateBook);
+
+router.delete('/book', bookController.deleteBook);
+
+module.exports = router;
